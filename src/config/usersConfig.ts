6b04f88e497229d44f1cffb0cafea0f7e9292459
Tech Service <tech.services@traffic-jet.com>
2025-09-10 11:47:36 +0300

    allow2

--- src/config/usersConfig.ts
+++ src/config/usersConfig.ts
@@ -1,7 +1,7 @@
 import type { Role } from "../types/domain";
 
 export const allowedUsers: Array<{ email: string; role: Role }> = [
-  { email: "hr@company.com", role: "hr" },
+  { email: "anna.hr@traffic-jet.com", role: "hr" },
   { email: "buyer@company.com", role: "buyer" },
   { email: "head@company.com", role: "head" },
 ];
